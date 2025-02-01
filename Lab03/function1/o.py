f0d7f770e6006c1ed8c27916ca10fc1c73c9a399
esrarustemoglu <e_rustemoglu@kbtu.kz>
2025-02-02 00:07:49 +0500

    update

---
+++ Lab03/function1/o.py
@@ -0,0 +1,11 @@
+from n import has_33, unique, histogram
+
+print(has_33([1, 3, 3])) 
+print(has_33([1, 3, 1, 3])) 
+print(has_33([3, 1, 3])) 
+
+my_list = [1, 2, 3, 1, 4, 5, 3]
+unique(my_list)
+
+lst = [4, 9, 7]
+histogram(lst)
