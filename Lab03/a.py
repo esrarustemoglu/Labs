28356bcbdf2dc0d0f910a175469ebdee9983d1f9
esrarustemoglu <e_rustemoglu@kbtu.kz>
2025-02-01 23:05:36 +0500

    update

--- Lab03/a.py
+++
@@ -1,8 +0,0 @@
-class string:
-    def getString(self):
-        self.s = input()
-    def printString(self):
-        print(self.s.upper())
-sentence = string()
-sentence.getString()
-sentence.printString()
